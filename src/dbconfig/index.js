722648037d02d2e1bf8c3aa2804b69200d82c03a
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2022-11-16 21:28:37 +0800

    change connection

--- src/dbconfig/index.js
+++ src/dbconfig/index.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'books',
+    host: 'ec2-54-147-36-107.compute-1.amazonaws.com',
+    user: 'apanrrqkhxsiwt',
+    password: '3528f2ab6996bcea28b785a457ea81f4e446e5b64fb867486e17ea8a52939717',
+    database: 'daav29qp7vd1d6',
+    port: '5432',
+    URL: 'postgres://apanrrqkhxsiwt:3528f2ab6996bcea28b785a457ea81f4e446e5b64fb867486e17ea8a52939717@ec2-54-147-36-107.compute-1.amazonaws.com:5432/daav29qp7vd1d6',
 });
 
 connection.connect((err) => {
