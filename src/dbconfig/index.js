fb070ba75e1d9b389d9dc6002cd9bc8317f7c8af
rizrmdhn <rizrmdhn.work@gmail.com>
2022-11-19 15:21:54 +0800

    fix mysql error connection

--- src/dbconfig/index.js
+++ src/dbconfig/index.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const dbConfig = {
   multipleStatements: true,
   host: 'remotemysql.com',
   user: 'byMWsNlDww',
   password: 'PLJiPdPZ32',
   database: 'byMWsNlDww',
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Mysql terkoneksi');
-});
+};
+
+const connection = mysql.createPool(dbConfig);
+
+connection.getConnection(function(error, getConnection) {
+      if (error) {
+        console.log('error when connectiong to db:', error);
+      } else {
+        console.log('Mysql terkoneksi');
+      }
+    });
+  
 
 module.exports = connection;
